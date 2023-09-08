5a5d3ad399899da5c8ce06887ae986bc22795752
Alen George <alengeorge9526@gmail.com>
2023-09-08 09:59:25 +0530

    Initial Commit

---
+++ customView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  customView
+//
+//  Created by Alen George on 08/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
